8714f9e47cba1e5b5821f8b63c6575d108a2afc6
Sangeui <sangeuis@icloud.com>
2021-11-01 00:39:24 +0900

    GroupAccessInterface 추가

---
+++ Corgi/Use Cases/Data Access Interface/GroupAccessInterface.swift
@@ -0,0 +1,31 @@
+//
+//  GroupAccessInterface.swift
+//  Corgi
+//
+//  Created by 서상의 on 2021/11/01.
+//
+
+import Foundation
+import CorgiStorage
+
+protocol GroupAccessInterface {
+    func read(predicate: NSPredicate?, sort: NSSortDescriptor?) -> [GroupRM]
+    func update(group: GroupRM) -> Bool
+    func delete(group: UUID) -> Bool
+}
+
+extension CoreDataInterface: GroupAccessInterface {
+    func read(predicate: NSPredicate?, sort: NSSortDescriptor?) -> [GroupRM] {
+        return self.readGroups(predicate: predicate, sort: sort)
+    }
+    
+    func update(group: GroupRM) -> Bool {
+        return self.updateGroup(group)
+    }
+    
+    func delete(group: UUID) -> Bool {
+        return self.deleteGroup(group)
+    }
+}
+
+
